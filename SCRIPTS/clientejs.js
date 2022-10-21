e3404ae9a845fbe33664a83e1fe50a0f6fa9d203
mikelmuru <102353831+mikelmuru@users.noreply.github.com>
2022-10-21 10:45:52 +0200

    Delete clientejs.js

--- SCRIPTS/clientejs.js
+++
@@ -1,6 +0,0 @@
-﻿function posicionate(elemento) {
-    document.getElementById(elemento).focus();
-    document.getElementById(elemento).style.color = '#0000FF';
-    document.getElementById(elemento).style.fontWeight = 'bold';
-
-}
